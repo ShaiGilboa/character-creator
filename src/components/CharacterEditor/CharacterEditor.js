0d3282d64f6ff54d0c2d00c9411ddac3743891f6
Shai Gilboa <52139362+ShaiGilboa@users.noreply.github.com>
2021-07-26 10:20:23 -0400

    complete

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -28,9 +28,19 @@ function App() {
     defaultClothesColor
   );
 
-  return (
+  return (<>
     <main className={styles.characterEditor}>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        <div className={styles.characterWrapper}>
+          <Character
+              body={body}
+              head={head}
+              face={face}
+              accessory={accessory}
+              skinColor={skinColor}
+              clothesColor={clothesColor}
+          />
+        </div>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
@@ -78,17 +88,10 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
+    <div className={styles.bg}/>
     </main>
+      </>
+
   );
 }
 
